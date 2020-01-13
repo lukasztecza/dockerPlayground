90a4af52268ec18d6eb779109384390a7ac0f896
lukasztecza <lukasztecza.pl@gmail.com>
2020-01-12 19:21:14 -0500

    redis done

---
+++ dockerPrivateRedis/password.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def randomStringwithDigitsAndSymbols(stringLength=10):
+    """Generate a random string of letters, digits and special characters """
+
+    password_characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(password_characters) for i in range(stringLength))
+
+print("Generating Random String password with letters, digits and special characters ")
+print ("First Random String ", randomStringwithDigitsAndSymbols(200) )
